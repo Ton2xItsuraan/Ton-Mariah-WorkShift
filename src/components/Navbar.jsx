f2c4fa6507f2f786ea43e6a252a4108f982727e2
mariahworking18 <mariah.working18@gmail.com>
2024-01-11 20:29:08 +0800

    FrontEnd 1st Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,156 @@
+import React, { Fragment, useState } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { BiChevronDown } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { AiOutlineClose, AiOutlineLogout } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import CustomButton from "./CustomButton";
+import { users } from "../utils/data.js";
+import { useSelector } from "react-redux";
+
+function MenuList({ user, onClick }) {
+  const handleLogout = () => {};
+
+  return (
+    <div>
+      <Menu as="div" className="inline-block text-left">
+        <div className=" flex">
+          <Menu.Button className="inline-flex gap-2 w-full rounded-md bg-white md:px-4 py-2 text-sm font-medium text-slate-700 hover:bg-opacity-20">
+            <div className="leading[80px] flex flex-col items-start">
+              <p className="text-   font-semibold">
+                {user?.firstName ?? user?.name}
+              </p>
+              <span className="text-sm text-blue-600">
+                {user?.jobTitle ?? user?.email}
+              </span>
+            </div>
+
+            <img
+              src={user?.profileUrl}
+              alt="user-profile"
+              className="w-10 h-10 rounded-full object-cover"
+            ></img>
+            <BiChevronDown
+              className="h-8 w-8 text-state-600 "
+              aria-hidden="true"
+            ></BiChevronDown>
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="trasition ease-out duration-100"
+          enterForm="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute z-50 right-2 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg focus:outline-none">
+            <div className="p-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <Link
+                    to={`${
+                      user?.accountType ? "user-profile" : "company-profile"
+                    }`}
+                    className={`${
+                      active ? "bg-blue-500 text-white" : "text-gray-900"
+                    } group flex w-full items-center rounded-md p-2 text-sm`}
+                    onClick={onClick}
+                  >
+                    <CgProfile
+                      className={`${
+                        active ? "text-white" : "text-gray-600"
+                      } mr-2 h-5 w-5`}
+                      aria-hidden="true"
+                    />
+                    {user?.accountType ? "User Profile" : "Comapany Profile"}
+                  </Link>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    onClick={() => handleLogout()}
+                    className={`${
+                      active ? "bg-blue-500 text-white" : "text-gray-900"
+                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                  >
+                    <AiOutlineLogout
+                      className={`${
+                        active ? "text-white" : "text-gray-600"
+                      } mr-2 h-5 w-5  `}
+                      aria-hidden="true"
+                    />
+                    Log Out
+                  </button>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  );
+}
+
+const Navbar = () => {
+  const user = users[1];
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleCloseNavbar = () => {
+    setIsOpen((prev) => !prev);
+  };
+  return (
+    <>
+      <div className="relative bg-white z-50">
+        <nav className="container mx-auto flex items-center justify-between p-5">
+          <div>
+            <Link to="/" className="text-blue-600 font-bold text-xl">
+              Work<span className="text-black">Shift</span>
+            </Link>
+          </div>
+          <ul className="hidden lg:flex gap-10 text-base">
+            <li>
+              <Link to="/">Find Job</Link>
+            </li>
+            <li>
+              <Link to="/">Companies</Link>
+            </li>
+            <li>
+              <Link to="/upload-job">Upload Job</Link>
+            </li>
+            <li>
+              <Link to="/about-us">About</Link>
+            </li>
+          </ul>
+
+          <div className="hidden lg:block">
+            {!user?.token ? (
+              <Link to="/user-auth">
+                <CustomButton
+                  title="Sign-in"
+                  containerStyles="text-blue-600 py-1.5 px-5 focus:outlione-none hover:bg-blue-800 hover:text-white rounded-full text-base border border-blue-600"
+                />
+              </Link>
+            ) : (
+              <div>
+                <MenuList user={user} />
+              </div>
+            )}
+            <Link to="/user-auth"></Link>
+          </div>
+        </nav>
+        {/* mobile menue */}
+        <div
+          className={`${
+            isOpen ? "absolute flex bg-[#f7fdfd] " : "hidden"
+          } container mx-auto lg:hidden flex-col pl-8 gap-3 py-5`}
+        ></div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
