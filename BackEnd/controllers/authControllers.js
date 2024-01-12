de46520d2b4ef132e382d46570ca1915a029f674
Ton2xItsuraan <vantoineludel@gmail.com>
2024-01-11 22:07:03 -0600

    finished controllers

--- BackEnd/controllers/authControllers.js
+++ BackEnd/controllers/authControllers.js
@@ -1,3 +1,4 @@
+import Users from "../models/userModel.js";
 
 
 export const register = async(req, res, next) => {
@@ -5,10 +6,92 @@ export const register = async(req, res, next) => {
 // validate fields
 
 if (!firstName) {
-    
+    next("First Name is required");
+
 }
+if(!email) {
+    next("Email is required")
+}
+if (!lastName) {
+    next("Last Name is required")
+};
+if (!password) {
+    next("Password is required")
+};
+
+try {
+    const userExist = await Users.findOne({ email });
+
+    if(userExist){
+        next("Email address already exists");
+        return;
+    }
+    const user = await Users.create({
+        firstName,
+        lastName,
+        email,
+        password,
+    });
+
+    // user token
+    const token = user.createJWT();
+    res.status(201).send({
+        success: true,
+        message: "Account created successfully",
+        user: {
+            _id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            accountType: user.accountType,
+        },
+        token,
+    })
+} catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });   
+}
+
 };
 
 export const signIn = async(req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        //validation
+        if(!email || !password) {
+            next("Please Provide use credentials");
+            return;
+        }
 
+        //find user by email
+        const user = await User.findOne({ email }.select("+password"));
+
+        if(!user) {
+            next("Invalid email or password");
+            return;
+        }
+
+        //compare password
+        const isMatch = await user.comparePassword(password);
+
+        if(!isMatch) {
+            next("Invalid email or password");
+            return;
+        }
+
+        user.password = undefined;
+
+        const token = user.createJWT();
+
+        res.status(201).json({
+            success: true,
+            message: "Login successfully",
+            user,
+            token,
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({ message: error.message})
+    }
 };
